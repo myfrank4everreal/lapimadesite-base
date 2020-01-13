f2a1c59b9b34e3ffc1cb5a81811326b5fcf48c17
myfrank4everreal <myfrank4everrel@gmail.com>
2020-01-13 06:46:17 +0400

    this is the official version 1 of the affiliate site

--- store/models.py
+++ store/models.py
@@ -5,8 +5,13 @@ class Products(models.Model):
     name = models.CharField(max_length=40)
     desc = models.CharField(max_length=300)
     price = models.FloatField(default=1)
+    amz_link = models.CharField(max_length=300, null=True, blank=True)
     
 
     def __str__(self):
-        return self.name    
+        return self.name
 
+    
+    def shotend_desc(self):
+        return self.desc[:200] + '...'
+   
