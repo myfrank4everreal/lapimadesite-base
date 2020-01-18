394868323819746ed03e82155f65f7825169b9ca
myfrank4everreal <myfrank4everrel@gmail.com>
2020-01-19 00:45:34 +0400

    made some in corrections to the service.html

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'affiliate/static'),
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '') #remember to add media to path before pushin to heroku
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') #remember to add media to path before pushin to heroku
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
