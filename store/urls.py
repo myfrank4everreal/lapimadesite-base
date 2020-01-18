4644fdb4b31cb4790d4cc6f7397e401ed95dd832
myfrank4everreal <myfrank4everrel@gmail.com>
2020-01-19 00:17:34 +0400

    the commit with a functional nav items page like about services etc

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('services', views.Services, name= 'services' ),
+    path('about', views.About, name= 'about' ),
+    path('contact', views.addContact, name= 'contact' ),
+    path('blog/', views.Blog, name= 'blog' ),
+   
+  
 ]
--- store/views.py
+++ store/views.py
@@ -8,3 +8,24 @@ def index(request):
     products = Products.objects.all()
     context = {'products':products}
     return render(request, 'store/index.html', context)
+
+
+def Services(request):
+    context = {}
+    return render(request, 'store/Services.html', context)
+
+
+
+def addContact(request):
+    context = {}
+    return render(request, 'store/addContact.html', context)
+
+
+def Blog(request):
+    context = {}
+    return render(request, 'store/Blog.html', context)
+
+
+def About(request):
+    context = {}
+    return render(request, 'store/about.html', context)
