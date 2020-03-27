d341acb7e178f1bebf19e7ca2ea1cef558166a93
myfrank4everreal <myfrank4everrel@gmail.com>
2020-03-27 23:05:36 +0400

    just fixed the s3 bucket

--- affiliate/settings.py
+++ affiliate/settings.py
@@ -162,3 +162,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_HOST = 'us-east-2.amazonaws.com'
+AWS_S3_REGION_NAME = 'us-east-2'
+
+AWS_S3_SIGNATURE_VERSION = 's3v4'
